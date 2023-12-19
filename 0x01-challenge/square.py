8acf07faccca70ba7b7fe37642d713c0330a84d6
Abel <aatsou12@gmail.com>
2023-12-19 14:15:28 +0000

    done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,49 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""A class representing a square."""
+class Square:
+    """A class representing a square."""
 
-class square():
-    
-    width = 0
-    height = 0
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a square with given width and height.
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        Args:
+            width (int): The width of the square.
+            height (int): The height of the square.
+        """
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """
+        Calculate the area of the square.
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        Returns:
+            int: The calculated area of the square.
+        """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """
+        Calculate the perimeter of the square.
+
+        Returns:
+            int: The calculated perimeter of the square.
+        """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """
+        String representation of the square.
+
+        Returns:
+            str: Details about the width and height of the square.
+        """
+        return f"Width: {self.width}, Height: {self.height}"
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
+
